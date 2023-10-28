b54261d0983497523886726c1a481efc7adb17db
Larry <larry@projectcolvin.com>
2023-10-28 15:39:23 +0100

    First commit of working prototype
    - rapidRTOS manager task
    - rapidRTOS plugin parent class

---
+++ rapidRTOS.h
@@ -0,0 +1,265 @@
+/**
+ * @file rapidRTOS.h
+ * @author Larry Colvin (PCLabTools@github)
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-22
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef rapidRTOS_h
+#define rapidRTOS_h
+
+#include <Arduino.h>
+#include <FreeRTOS.h>
+#include "task.h"
+#include "queue.h"
+#include "timers.h"
+
+#define rapidPASS 1
+#define rapidFAIL 0
+
+/**
+ * @brief rapidDebug type enum for categorising debug
+ * messages for printing to the debug console
+ * 
+ */
+enum class rapidDebug
+{
+  ERROR,
+  WARNING,
+  INFO
+};
+
+/**
+ * @brief rapidFunction type struct for messaging to
+ * and from rapidPlugins
+ * 
+ */
+struct rapidFunction
+{
+  char function[16];
+  char parameters[48];
+};
+
+#include "rapidRTOS_manager.h"
+
+#ifndef rapidRTOS_DEFAULT_STACK_SIZE
+#define rapidRTOS_DEFAULT_STACK_SIZE 128        // Default stack size used by rapidPlugins if not specified during run
+#endif
+
+#ifndef rapidRTOS_DEFAULT_INTERFACE_BUFFER
+#define rapidRTOS_DEFAULT_INTERFACE_BUFFER 64   // Default interface buffer size for rapidFunction commands
+#endif
+
+#ifndef rapidRTOS_DEFAULT_QUEUE_SIZE
+#define rapidRTOS_DEFAULT_QUEUE_SIZE 1          // Default interface incoming queue size for rapidFunctions
+#endif
+
+#ifndef rapidRTOS_DEFAULT_PRIORITY
+#define rapidRTOS_DEFAULT_PRIORITY 1            // Default task priority if not specified during run
+#endif
+
+/**
+ * @brief rapidPlugin class provides a parent definition of a plugin
+ * that can be used by derived classes to simplify setting up
+ * and running a task in the rapidRTOS framework
+ * 
+ */
+class rapidPlugin
+{
+  public:
+    rapidPlugin();
+    ~rapidPlugin();
+    BaseType_t run(TaskFunction_t child, uint32_t stackDepth = rapidRTOS_DEFAULT_STACK_SIZE, int queueSize = rapidRTOS_DEFAULT_QUEUE_SIZE, UBaseType_t priority = rapidRTOS_DEFAULT_PRIORITY);
+    BaseType_t runCore(UBaseType_t core, TaskFunction_t child, uint32_t stackDepth = rapidRTOS_DEFAULT_STACK_SIZE, int queueSize = rapidRTOS_DEFAULT_QUEUE_SIZE, UBaseType_t priority = rapidRTOS_DEFAULT_PRIORITY);
+    void stop();
+    const char* cmd(const char* command, TickType_t timeout = portMAX_DELAY);
+    virtual uint8_t interface(rapidFunction incoming, char messageBuffer[]);
+
+  protected:
+    const char* _pID;                     // main task name
+    char _iID[32];                        // interface task name buffer
+    TaskHandle_t _taskHandle = NULL;      // main task handle reference
+    TaskHandle_t _interfaceHandle = NULL; // interface task handle reference
+    QueueHandle_t _taskQueue = NULL;      // interface task incoming queue
+    QueueHandle_t _taskResponse = NULL;   // interface task outgoing queue
+    static void interface_loop(void*);
+};
+
+/**
+ * @brief Construct a new rapid Plugin::rapid Plugin object
+ * 
+ */
+rapidPlugin::rapidPlugin()
+{
+  //
+}
+
+/**
+ * @brief Destroy the rapid Plugin::rapid Plugin object
+ * 
+ */
+rapidPlugin::~rapidPlugin()
+{
+  //
+}
+
+/**
+ * @brief Runs a rapidPlugin task and registers the task with the rapidRTOS manager
+ * 
+ * @param child reference to task of plugin
+ * @param stackDepth stack depth of the task (stack size is target dependent)
+ * @param queueSize queue size for rapidFunction commands between plugins
+ * @param priority FreeRTOS task priority
+ * @return BaseType_t 1 = task started and registered | 0 = task failed to start
+ */
+BaseType_t rapidPlugin::run(TaskFunction_t child, uint32_t stackDepth, int queueSize, UBaseType_t priority)
+{
+  sprintf(_iID, "i_%s", _pID);
+  if (!rapidRTOS.getTaskHandle(_pID))
+  {
+    _taskQueue = xQueueCreate(queueSize, sizeof(const char*));
+    _taskResponse = xQueueCreate(1, sizeof(const char*));
+    if(xTaskCreate(child, _pID, stackDepth, this, priority, &_taskHandle)\
+    && xTaskCreate(&interface_loop, _iID, 512, this, priority, &_interfaceHandle))\
+    return (BaseType_t)rapidRTOS.reg(_taskHandle, &_taskQueue, &_taskResponse);
+    else
+    {
+      vTaskDelete(_taskHandle);
+      _taskHandle = NULL;
+      vQueueDelete(_taskQueue);
+      _taskQueue = NULL;
+      vQueueDelete(_taskResponse);
+      _taskResponse = NULL;
+    return 0;
+    }
+  }
+  return 0;
+}
+
+/**
+ * @brief Runs a rapidPlugin task on the specified core and registers the task with the rapidRTOS manager
+ * 
+ * @param core core id
+ * @param child reference to task of plugin
+ * @param stackDepth stack depth of the task (stack size is target dependent)
+ * @param queueSize queue size for rapidFunction commands between plugins
+ * @param priority FreeRTOS task priority
+ * @return BaseType_t BaseType_t 1 = task started and registered | 0 = task failed to start
+ */
+BaseType_t rapidPlugin::runCore(UBaseType_t core, TaskFunction_t child, uint32_t stackDepth, int queueSize, UBaseType_t priority)
+{
+  sprintf(_iID, "i_%s", _pID);
+  if (!rapidRTOS.getTaskHandle(_pID))
+  {
+    _taskQueue = xQueueCreate(queueSize, sizeof(const char*));
+    _taskResponse = xQueueCreate(1, sizeof(const char*));
+    if(xTaskCreateAffinitySet(child, _pID, stackDepth, this, priority, core, &_taskHandle)\
+    && xTaskCreateAffinitySet(&interface_loop, _iID, 512, this, priority, core, &_interfaceHandle))\
+    return (BaseType_t)rapidRTOS.reg(_taskHandle, &_taskQueue, &_taskResponse);
+    else
+    {
+      vTaskDelete(_taskHandle);
+      _taskHandle = NULL;
+      vQueueDelete(_taskQueue);
+      _taskQueue = NULL;
+      vQueueDelete(_taskResponse);
+      _taskResponse = NULL;
+    return 0;
+    }
+  }
+  return 0;
+}
+
+/**
+ * @brief Stops a running task
+ * 
+ */
+void rapidPlugin::stop()
+{
+  if (_taskHandle)
+  {
+    if (_interfaceHandle) { vTaskDelete(_interfaceHandle); }
+    vTaskDelete(_taskHandle);
+    _taskHandle = NULL;
+    rapidRTOS.dereg(_pID);
+    vQueueDelete(_taskQueue);
+    _taskQueue = NULL;
+    vQueueDelete(_taskResponse);
+    _taskResponse = NULL;
+  }
+}
+
+/**
+ * @brief Sends a rapidFunction command to the plugin
+ * 
+ * @param command string literal containing the rapidFunction command
+ * @param timeout maximum wait time for response from command
+ * @return const char* response from command
+ */
+const char* rapidPlugin::cmd(const char* command, TickType_t timeout)
+{
+  xQueueSend(_taskQueue, &command, timeout);
+  const char* response = "";
+  xQueueReceive(_taskResponse, &response, timeout);
+  return response;
+}
+
+/**
+ * @brief Virtual function to be overriden in child implementations. 
+ * This function is to be used for creating states that are called
+ * when rapidFunction commands are received
+ * 
+ * @param incoming rapidFunction command received
+ * @param messageBuffer response message buffer
+ * @return uint8_t 1 = function run | 0 = no function
+ */
+uint8_t rapidPlugin::interface(rapidFunction incoming, char messageBuffer[])
+{
+  return 0;
+}
+
+/**
+ * @brief Interface loop used to process incoming rapidFunction commands
+ * 
+ * @param pModule pointer to calling object
+ */
+void rapidPlugin::interface_loop(void* pModule)
+{
+  rapidPlugin* plugin = (rapidPlugin*)pModule;
+  char messageBuffer[rapidRTOS_DEFAULT_INTERFACE_BUFFER] = "";
+  const char* incomingMessage = messageBuffer;
+  const char* outgoingMessage = messageBuffer;
+  for ( ;; )
+  {
+    xQueueReceive(plugin->_taskQueue, &incomingMessage, portMAX_DELAY);
+    sprintf(messageBuffer, ""); // clear message buffer for outgoing message
+    rapidFunction incoming = rapidRTOS.parse(incomingMessage);
+    do
+    {
+      if (!strcmp(incoming.function, "identity"))
+      {
+        sprintf(messageBuffer, "%s", plugin->_pID);
+        continue;
+      }
+      if (!strcmp(incoming.function, "stop"))
+      {
+        plugin->stop();
+        break;
+      }
+      if (plugin->interface(incoming, messageBuffer))
+      {
+        continue;
+      }
+      sprintf(messageBuffer, "unknown_function(%s)", incoming.function);
+      rapidRTOS.printDebug(1, rapidDebug::ERROR, "%s: unknown_function(%s)\n", plugin->_pID, incoming.function);
+    } while (false);
+    outgoingMessage = messageBuffer;
+    xQueueSend(plugin->_taskResponse, &outgoingMessage, portMAX_DELAY);
+  }
+}
+
+#endif
---
+++ rapidRTOS_manager.h
@@ -0,0 +1,441 @@
+/**
+ * @file rapidRTOS_manager.h
+ * @author Larry Colvin (PCLabTools@github)
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-28
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef rapidRTOS_manager_h
+#define rapidRTOS_manager_h
+
+#ifndef rapidRTOS_MAX_MODULES
+/**
+ * @brief Max number of modules the manager can store.
+ * Increasing this number will increase the overall memory
+ * used by the manager.
+ * 
+ */
+#define rapidRTOS_MAX_MODULES 10
+#endif
+
+/**
+ * @brief rapidRTOS manager class provides a 'singleton' class for a rapidRTOS
+ * task management object. Using this class will allow for
+ * centralised and communicable paralell tasks to be managed.
+ * 
+ */
+class rapidRTOS_manager
+{
+  public:
+    static rapidRTOS_manager& getInstance() 
+    {
+      // Allocate with `new` in case Singleton is not trivially destructible.
+      static rapidRTOS_manager* singleton = new rapidRTOS_manager();
+      return *singleton;
+    }
+    uint8_t reg(TaskHandle_t taskHandle, QueueHandle_t* command, QueueHandle_t* response);
+    uint8_t dereg(const char* taskName);
+    const char* cmd(const char* taskName, const char* command, TickType_t timeout = portMAX_DELAY);
+    uint8_t setDebugLevel(uint8_t debugLevel);
+    void setDebugStream(Stream* streamRef);
+    template<typename... Args> void printDebug(uint8_t debugLevel, const char* message, Args... args);
+    template<typename... Args> void printDebug(uint8_t debugLevel, rapidDebug messageType, const char* message, Args... args);
+    TaskHandle_t getTaskHandle(const char* taskName);
+    TaskStatus_t getTaskStatus(const char* taskName);
+    uint8_t getNumTasks();
+    void printTaskStatus(const char* taskName);
+    rapidFunction parse(const char* message);
+
+  private:
+    rapidRTOS_manager();
+    rapidRTOS_manager(const rapidRTOS_manager&) = delete;
+    rapidRTOS_manager& operator=(const rapidRTOS_manager&) = delete;
+    rapidRTOS_manager(rapidRTOS_manager&&) = delete;
+    rapidRTOS_manager& operator=(rapidRTOS_manager&&) = delete;
+    static void managerTask(void* pParameters);
+    const char* _taskNames[rapidRTOS_MAX_MODULES];
+    TaskHandle_t _taskHandles[rapidRTOS_MAX_MODULES];
+    QueueHandle_t* _taskQueues[rapidRTOS_MAX_MODULES];
+    QueueHandle_t* _responseQueues[rapidRTOS_MAX_MODULES];
+    uint8_t _debugLevel = 0;
+    Stream* _debugStream = &Serial;
+    TaskHandle_t _managerHandle;
+};
+
+/**
+ * @brief Construct a new rapidRTOS manager::rapidRTOS manager object and
+ * initialises _taskNames as blank tasks
+ * 
+ */
+rapidRTOS_manager::rapidRTOS_manager()
+{
+  for (size_t i = 0; i < rapidRTOS_MAX_MODULES; i++)
+  {
+    _taskNames[i] = "";
+  }
+  #ifndef rapidRTOS_DISABLE_MANAGER
+  xTaskCreate(managerTask, "rapidRTOS_manager", 256, this, 1, &_managerHandle);
+  #endif
+}
+
+/**
+ * @brief Registers a task with the manager which allows for searching
+ * and interacting with tasks through the manager interface
+ * 
+ * @param taskHandle task reference
+ * @param command command queue reference
+ * @param response response queue reference
+ * @return uint8_t 1 = no space for task registration | 0 = pass
+ */
+uint8_t rapidRTOS_manager::reg(TaskHandle_t taskHandle, QueueHandle_t* command, QueueHandle_t* response = NULL)
+{
+  TaskStatus_t taskStatus;
+  uint8_t freeStackSpace;
+  vTaskGetInfo(taskHandle, &taskStatus, freeStackSpace, eRunning);
+  for (size_t i = 0; i < rapidRTOS_MAX_MODULES; i++)
+  {
+    if(!strcmp(_taskNames[i],""))
+    {
+      _taskNames[i] = taskStatus.pcTaskName;
+      _taskHandles[i] = taskHandle;
+      _taskQueues[i] = command;
+      _responseQueues[i] = response;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+/**
+ * @brief Unregisters the task defined by task name
+ * 
+ * @param taskName string literal name of the task
+ * @return uint8_t 1 = task unregistered | 0 = task not found
+ */
+uint8_t rapidRTOS_manager::dereg(const char* taskName)
+{
+  for (size_t i = 0; i < rapidRTOS_MAX_MODULES; i++)
+  {
+    if(!strcmp(_taskNames[i],taskName))
+    {
+      _taskNames[i] = "";
+      _taskHandles[i] = NULL;
+      _taskQueues[i] = NULL;
+      _responseQueues[i] = NULL;
+      return rapidPASS;
+    }
+  }
+  return rapidFAIL;
+}
+
+/**
+ * @brief Sends a command to the task defined by task name
+ * 
+ * @param taskName string literal name of task
+ * @param command string literal containing the command
+ * @param timeout timeout for waiting for response
+ * @return const char* response to command
+ */
+const char* rapidRTOS_manager::cmd(const char* taskName, const char* command, TickType_t timeout)
+{
+  const char* response = "";
+  for (size_t i = 0; i < rapidRTOS_MAX_MODULES; i++)
+  {
+    if(!strcmp(_taskNames[i],taskName))
+    {
+      xQueueSend(*_taskQueues[i], &command, timeout);
+      xQueueReceive(*_responseQueues[i], &response, timeout);
+      break;
+    }
+  }
+  return response;
+}
+
+/**
+ * @brief Sets the debug level which is used to filter debug messages for printing
+ * 
+ * @param debugLevel 0 = off | >0 = >debug detail
+ * @return uint8_t current debug level
+ */
+uint8_t rapidRTOS_manager::setDebugLevel(uint8_t debugLevel)
+{
+  _debugLevel = debugLevel;
+  return _debugLevel;
+}
+
+/**
+ * @brief Sets the stream to use for debug message printing
+ * 
+ * @param streamRef stream reference
+ * @return uint8_t 
+ */
+void rapidRTOS_manager::setDebugStream(Stream* streamRef)
+{
+  _debugStream = streamRef;
+}
+
+/**
+ * @brief If the input debug level is greater than the manager's debug level
+ * a debug message is printed to the debug stream
+ * 
+ * @tparam Args any number of arguments of any type
+ * @param debugLevel debug level of the message
+ * @param message string literal containing message and escape characters for referencing a parameter argument
+ * @param args message parameter arguments
+ */
+template<typename... Args>
+void rapidRTOS_manager::printDebug(uint8_t debugLevel, const char* message, Args... args)
+{
+  printDebug(debugLevel, (rapidDebug)255, message, args...);
+}
+
+/**
+ * @brief If the input debug level is greater than the manager's debug level
+ * a debug message is printed to the debug stream
+ * 
+ * @tparam Args any number of arguments of any type
+ * @param debugLevel debug level of the message
+ * @param messageType type of debug message to categorise debug message
+ * @param message string literal containing message and escape characters for referencing a parameter argument
+ * @param args message parameter arguments
+ */
+template<typename... Args> void
+rapidRTOS_manager::printDebug(uint8_t debugLevel, rapidDebug messageType, const char* message, Args... args)
+{
+  if (_debugLevel >= debugLevel)
+  {
+    switch (messageType)
+    {
+      case rapidDebug::ERROR:
+        #ifdef ANSI_OUTPUT
+        _debugStream->print("\033[41m[ERROR]");
+        #else
+        _debugStream->print("[ERROR]");
+        #endif
+        break;
+      case rapidDebug::WARNING:
+        #ifdef ANSI_OUTPUT
+        _debugStream->print("\033[43m[WARNING]");
+        #else
+        _debugStream->print("[WARNING]");
+        #endif
+        break;
+      case rapidDebug::INFO:
+        #ifdef ANSI_OUTPUT
+        _debugStream->print("\033[7m[INFO]");
+        #else
+        _debugStream->print("[INFO]");
+        #endif
+        break;
+      default:
+        #ifdef ANSI_OUTPUT
+        _debugStream->print("\033[46m[DEBUG]");
+        #else
+        _debugStream->print("[DEBUG]");
+        #endif
+        break;
+    }
+    #ifdef ANSI_OUTPUT
+    _debugStream->print("\033[0m");
+    #endif
+    _debugStream->printf(" (%d) ", millis());
+    _debugStream->printf(message, args...);
+  }
+}
+
+/**
+ * @brief Requests the task handle of the task defined by task name
+ * 
+ * @param taskName string literal containing task name
+ * @return TaskHandle_t task handle reference of task if found
+ */
+TaskHandle_t rapidRTOS_manager::getTaskHandle(const char* taskName)
+{
+  for (uint8_t i = 0 ; i < rapidRTOS_MAX_MODULES ; i++)
+  {
+    if(!strcmp(_taskNames[i],taskName))
+    {
+      return _taskHandles[i];
+    }
+  }
+  return NULL;
+}
+
+/**
+ * @brief Requests the task status of the task defined by the task name
+ * 
+ * @param taskName string literal containing task name
+ * @return TaskStatus_t task status of the task if found
+ */
+TaskStatus_t rapidRTOS_manager::getTaskStatus(const char* taskName)
+{
+  TaskStatus_t taskstatus;
+  uint8_t freestackspace;
+  for (uint8_t i = 0 ; i < rapidRTOS_MAX_MODULES ; i++)
+  {
+    if(!strcmp(_taskNames[i],taskName))
+    {
+      vTaskGetInfo(_taskHandles[i], &taskstatus, freestackspace, eInvalid);
+    }
+  }
+  return taskstatus;
+}
+
+/**
+ * @brief Gets the number of registered tasks
+ * 
+ * @return uint8_t number of registered tasks
+ */
+uint8_t rapidRTOS_manager::getNumTasks()
+{
+  uint8_t numTasks = 0;
+  for (uint8_t i = 0 ; i < rapidRTOS_MAX_MODULES ; i++)
+  {
+    if(strcmp(_taskNames[i],""))
+    {
+      numTasks++;
+    }
+  }
+  return numTasks;
+}
+
+/**
+ * @brief Prints the current task status to the debug stream
+ * 
+ * @param taskName string literal containing the task name
+ */
+void rapidRTOS_manager::printTaskStatus(const char* taskName)
+{
+  TaskStatus_t taskStatus = getTaskStatus(taskName);
+  const char * state = "";
+  switch (taskStatus.eCurrentState)
+  {
+    case eRunning:
+      state = "Running";
+      break;
+    case eReady:
+      state = "Ready";
+      break;
+    case eBlocked:
+      state = "Blocked";
+      break;
+    case eSuspended:
+      state = "Suspended";
+      break;
+    case eDeleted:
+      state = "Deleted";
+      break;
+    case eInvalid:
+      state = "Invalid";
+      break;
+    default:
+      state = "Invalid";
+      break;
+  }
+  if (!strcmp(taskStatus.pcTaskName,taskName))
+  {
+    _debugStream->printf ("***** TASK STATUS *****\n\
+Name:     %s\n\
+State:    %s[%d]\n\
+Address:  %p\n\
+Runtime:  %d\n\
+Priority: %d\n\
+Task ID:  %d\n\
+***********************\n",\
+    taskStatus.pcTaskName, state, taskStatus.eCurrentState, taskStatus.pxStackBase, taskStatus.ulRunTimeCounter, taskStatus.uxCurrentPriority, taskStatus.xTaskNumber);
+  }
+  else
+  {
+    _debugStream->printf ("***** TASK STATUS *****\n\
+Name:     %s\n\
+State:    %s[%d]\n\
+Address:  %p\n\
+Runtime:  %d\n\
+Priority: %d\n\
+Task ID:  %d\n\
+***********************\n",\
+    taskName, "Invalid", eInvalid, 0, 0, 0, 0);
+  }
+}
+
+/**
+ * @brief Function to be used to parse rapidFunction messages from a string literal
+ * 
+ * @param message string literal containing rapidFunction message
+ * @return rapidFunction parsed rapidFunction
+ */
+rapidFunction rapidRTOS_manager::parse(const char* message)
+{
+  rapidFunction returnMessage;
+  int functionIndex = 0;
+  int parameterIndex = 0;
+  bool functionComplete = false;
+  bool messageComplete = false;
+  while (message[0] != '\0' && !messageComplete)
+  {
+    if (!functionComplete)
+    {
+      returnMessage.function[functionIndex] = message[0];
+      functionIndex++;
+      message++;
+      if (message[0] == ')')
+      {
+        messageComplete = true;
+      }
+      if (message[0] == '(')
+      {
+        returnMessage.function[functionIndex] = '\0';
+        functionComplete = true;
+      } 
+    }
+    else
+    {
+      if (message[0] == '(')
+      {
+        message++;
+      }
+      if(message[0] != ')')
+      {
+        returnMessage.parameters[parameterIndex] = message[0];
+        parameterIndex++;
+        message++;
+      }
+      if (message[0] == ')')
+      {
+        returnMessage.parameters[parameterIndex] = '\0';
+        messageComplete = true;
+        message++;
+      }
+    }
+  }
+  returnMessage.function[functionIndex] = '\0';
+  returnMessage.parameters[parameterIndex] = '\0';
+  return returnMessage;
+}
+
+/**
+ * @brief This is the manager's own task used to monitor critical tasks
+ * 
+ * @param pParameters void* pointer to calling object
+ */
+void rapidRTOS_manager::managerTask(void* pParameters)
+{
+  rapidRTOS_manager* plugin = (rapidRTOS_manager*)pParameters;
+  for (;;)
+  {
+    plugin->printDebug(255, rapidDebug::INFO, "Running in ULTRA debug mode");
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+  }
+}
+
+/**
+ * @brief External interface to the radpiRTOS_manager class singleton object
+ * 
+ */
+rapidRTOS_manager& rapidRTOS = rapidRTOS_manager::getInstance();
+
+#endif
